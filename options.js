aab2efd456c96c021b3792724444435dd645dcd1
rmin@andrew.cmu.edu <rmin@unix3.andrew.cmu.edu>
2017-02-11 01:32:14 -0500

    Add options page (HTML, JS)

--- options.js
+++ options.js
@@ -4,16 +4,20 @@ var HOTKEY_ENTRY_LAST_ROW_SELECTOR = '#hotkey_entry tr:last-child';
 var HOTKEY_ENTRY_DELETE_SELECTOR = 'button.delete';
 var INPUT_SELECTOR = 'input';
 var INPUT_TEXT_SELECTOR = 'input[type="text"]';
+var INPUT_CHECK_SELECTOR = 'input[type="checkbox"]';
+var INPUT_DOMAIN_SELECTOR = 'input[name="domain"]';
+var INPUT_HOTKEY_SELECTOR = 'input[name="hotkey"]';
+var INPUT_DEDUPLICATE_SELECTOR = 'input[name="deduplicate"]';
 var OPTIONS_FORM_SELECTOR = '#options';
-var ADD_HOTKEY_ENTRY_BUTTON_SELECTOR = '#add_daily_task';
+var ADD_HOTKEY_ENTRY_BUTTON_SELECTOR = '#add_hotkey';
 var SAVE_BUTTON_SELECTOR = '#save';
 var DISABLED = 'disabled';
 
 var HOTKEY_ENTRY_HTML = ' \
     <tr> \
-        <td><input required type="text" maxlength="50"></td> \
-        <td><input required type="number" min="0" max="1440"></td> \
-        <td><input required type="time"></td> \
+        <td><input required type="text" name="domain"></td> \
+        <td><input required type="text" maxlength="5" name="hotkey"></td> \
+        <td><input type="checkbox" name="deduplicate"></td> \
         <td><button class="delete">Delete</button></td> \
     </tr> \
 ';
@@ -29,38 +33,36 @@ function addHotkeyEntry() {
 }
 
 function getHotkeyEntrys() {
-    var dailyTasks = [];
+    var hotKeys = [];
     $(HOTKEY_ENTRY_ROWS_SELECTOR).each(function() {
         var jqThis = $(this);
-        var name = jqThis.find(INPUT_TEXT_SELECTOR).val();
-        var duration = parseInt(jqThis.find(INPUT_NUMBER_SELECTOR).val());
-        var completedBy =
-                timeStringToMinutes(jqThis.find(INPUT_TIME_SELECTOR).val());
-        dailyTasks.push({
-            name: name,
-            duration: duration,
-            completedBy: completedBy
+        var domain = jqThis.find(INPUT_DOMAIN_SELECTOR).val();
+        var hotkey = jqThis.find(INPUT_HOTKEY_SELECTOR).val();
+        var deduplicate = jqThis.find(INPUT_CHECK_SELECTOR).val();
+        hotKeys.push({
+            domain: domain,
+            hotkey: hotkey,
+            deduplicate: deduplicate
         });
     });
-    return dailyTasks;
+    return hotKeys;
 }
 
-function restoreHotkeyEntrys(dailyTasks) {
-    for (var i = 0; i < dailyTasks.length; i++) {
+function restoreHotkeyEntrys(hotKeys) {
+    for (var i = 0; i < hotKeys.length; i++) {
         addHotkeyEntry();
         var jqHotkeyEntryRow = $(HOTKEY_ENTRY_LAST_ROW_SELECTOR);
-        jqHotkeyEntryRow.find(INPUT_TEXT_SELECTOR).val(dailyTasks[i].name);
-        jqHotkeyEntryRow.find(INPUT_NUMBER_SELECTOR).val(dailyTasks[i].duration);
-        jqHotkeyEntryRow.find(INPUT_TIME_SELECTOR).val(
-                minutesToTimeString(dailyTasks[i].completedBy));
+        jqHotkeyEntryRow.find(INPUT_DOMAIN_SELECTOR).val(hotKeys[i].domain);
+        jqHotkeyEntryRow.find(INPUT_HOTKEY_SELECTOR).val(hotKeys[i].hotkey);
+        jqHotkeyEntryRow.find(INPUT_DEDUPLICATE_SELECTOR).val(hotKeys[i].deduplicate);
     }
 }
 
 // Saves options to chrome.storage.sync.
 function saveOptions() {
-    var dailyTasks = getHotkeyEntrys();
+    var hotKeys = getHotkeyEntrys();
     chrome.storage.sync.set({
-        dailyTasks: dailyTasks,
+        hotKeys: hotKeys
     }, function() {
         // Disable save button to indicate that options are saved.
         $(SAVE_BUTTON_SELECTOR).prop(DISABLED, true);
@@ -72,9 +74,9 @@ function saveOptions() {
 function restoreOptions() {
     // Default values.
     chrome.storage.sync.get({
-        dailyTasks: [],
+        hotKeys: []
     }, function(items) {
-        restoreHotkeyEntrys(items.dailyTasks);
+        restoreHotkeyEntrys(items.hotKeys);
     });
 }
 
