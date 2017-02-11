753a968d4869551fae85db51d952360a7e432581
Jemmin Chang <jchang504@gmail.com>
2017-02-11 02:12:58 -0500

    Move tabs.js to background.js

--- background.js
+++ background.js
@@ -1,6 +1,52 @@
+/*
+ * url: the string that includes the protocol and domain name of the site you want to go to
+ * (e.g. "https://google.com"). Must include the "www." if applicable.
+ * deduplicate: true if you want to check for a duplicate and go to it if it exists instead of
+ * creating a new tab. false if you want to create a new tab regardless of pre-existing tabs.
+ */
+function checked_new_tab(url, deduplicate){
+
+  chrome.tabs.query({}, function(tabs){
+
+    var domain_regex = new RegExp('^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)', 'i');
+    var curr_domain = domain_regex.exec(url)[1];
+    if(dup){
+      for (tab of tabs){
+        var tab_url = tab.url;
+        var tab_domain = domain_regex.exec(tab_url)[1];
+        if(curr_domain === tab_domain){
+          var tab_id = tab.id;
+          chrome.tabs.update(tab_id, {"active":true});
+          return;
+        };
+      }
+    }
+    chrome.tabs.create({"url": url});
+
+  });
+
+}
+
+var mappings = {};
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log(sender.tab ?  "from a content script:" + sender.tab.url :
-                "from the extension");
-        console.log(request.key);
-});
+  function(request, sender, sendResponse) {
+    var hotkey = request.hotkey;
+    console.log(hotkey);
+    var isLower = true;
+    var alpha_regex = new RegExp('/^[A-Z]$')
+    for c of hotkey{
+      var check_value = alpha_regex.exec(c);
+      if(check_value){
+        isUpper = false;
+        break;
+      }
+    }
+
+    url = mappings.hotkey;
+    if(url){
+      checked_new_tab(url, isLower);
+    }
+
+  }
+);
