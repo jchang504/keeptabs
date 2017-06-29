a9465bb694091b4baa4df56f13e1a8ab7503e5a8
Roger Kim <roger.kim486@gmail.com>
2017-06-29 15:13:57 -0400

    LOCAL ONLY added dot files

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "jquery": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": 0
+    },
+
+    "globals": {
+        "ACTIVE": true,
+        "CURRENT_TAB_KEY": true,
+        "CURRENT_WINDOW": true,
+        "DEDUPLICATE_KEY": true,
+        "DOMAIN_KEY": true,
+        "FOCUSED": true,
+        "HOLDKEY_MSG":true,
+        "HOLD_KEY_DEFAULT": true,
+        "HOLD_KEY_KEY": true,
+        "HOTKEYS_DEFAULT":true,
+        "HOTKEYS_KEY": true,
+        "HOTKEY_HOLD_KEY_CODE":true,
+        "HOTKEY_KEY": true,
+        "HOTKEY_MSG":true,
+        "INDEX": true,
+        "KEYDOWN": true,
+        "KEYUP": true,
+        "LOG_INFO": true,
+        "MOVE_LEFT_KEYVAL":true,
+        "MOVE_RIGHT_KEYVAL": true,
+        "NAV_LEFT_KEYVAL":true,
+        "NAV_LEFT_SYMBOL":true,
+        "NAV_PREVIOUS_KEYVAL":true,
+        "NAV_RIGHT_KEYVAL":true,
+        "NAV_RIGHT_SYMBOL":true,
+        "REFRESH_MSG": true,
+        "SEARCH_NAV_MSG": true,
+        "SEARCH_URL": true,
+        "TAB_CLOSE_KEYVAL":true,
+        "TAB_CLOSE_SYMBOL":true,
+        "TAB_ID_KEY": true,
+        "TAB_PREVIOUS_KEYVAL": true,
+        "TAB_SEARCH_KEYVAL": true,
+        "UPDATE_HOLD_KEY_MSG": true,
+        "WINDOW_ID_KEY": true,
+        "chrome": true
+    }
+};
